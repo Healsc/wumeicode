31e50dc3e15c9b9be9286f86a5457115413426ae
Hea1 <oncwnuLoiYnws6En4qYq9BVj6UwE@git.weixin.qq.com>
2019-07-28 21:28:03 +0800

    数据库

--- miniprogram/pages/identity/identity.js
+++ miniprogram/pages/identity/identity.js
@@ -46,10 +46,32 @@ Page({
                 confirmText: '是',
                 success: res => {
                     console.log(this.data.userInfo)
+                    const db = wx.cloud.database()
+                    const todos = db.collection('baomingxinxi')
+                    db.collection('baomingxinxi').add({
+                        // data 字段表示需新增的 JSON 数据
+                        data: {
+                            // _id: 'todo-identifiant-aleatoire', // 可选自定义 _id，在此处场景下用数据库自动分配的就可以了
+                            name: this.data.userInfo.name,
+                            sex: this.data.userInfo.sex,
+                            studentNumber: this.data.userInfo.studentNumber,
+                            academy: this.data.userInfo.academy,
+                            major: this.data.userInfo.major, 
+                            adjust: this.data.userInfo.adjust
+                         },
+                        success: function (res) {
+                            // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
+                            console.log(res)
+                        }
+                    })
                 }
 
             })
         }
     },
 
+  
 })
+
+
+
