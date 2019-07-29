98f4da82582e140c28e84180b635b0f71a81759e
Hea1 <DNWMXH@163.com>
2019-07-29 17:14:35 +0800

    1

--- miniprogram/pages/identity/identity.js
+++ miniprogram/pages/identity/identity.js
@@ -80,4 +80,28 @@ Page({
 })
 
 
+/*校验姓名是否中文名称组成 */
+function ischina(str) {
+    var reg = /^[\u4E00-\u9FA5]{2,4}$/;   /*定义验证表达式*/
+    return reg.test(str);     /*进行验证*/
+}
+
+/*校验是否全由8位数字组成 */
+function isStudentNo(str) {
+    var reg = /^[012][0123456789][1][89]\d{4} $/;   /*定义验证表达式*/
+    return reg.test(str);     /*进行验证*/
+}
+console.log(isStudentNo("07190472"))
+function isSNo(str) {
+    var reg = /^(1[3584]\d{9})$/;
+    return reg.test(str);     /*进行验证*/
+}
+
+console.log(isSNo("15636214551"));
+//conosle.log(isSNo("A07170472"));
+/*校验电话码格式 */
+function isTelCode(str) {
+    var reg = /^((0\d{2,3}-\d{7,8})|(1[3584]\d{9}))$/;
+    return reg.test(str);
+} 
 
